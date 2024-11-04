2122b8bd9d5092e3f422537366d426c805f5ae55
Александр Афанасьев <knowthepain00@mail.ru>
2024-11-05 00:53:07 +0300

    v0.2

--- main/views.py
+++ main/views.py
@@ -5,7 +5,3 @@ from django.contrib.auth import logout
 def index(request):
     return render(request, 'main/index.html')
 
-
-def logout_view(request):
-    logout(request)
-    return redirect('/')
